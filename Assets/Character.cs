c4cb003edc04679dfb9af2f18222b059f0441d5a
nstovring <nstovring@live.dk>
2016-04-09 00:16:25 +0200

    Character implementation

---
+++ Assets/Character.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+   
+    public float range;
+    public float speed;
+    public GameObject attackRangeObject;
+
+    public Collider attackRangeCollider;
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    attackRangeCollider = attackRangeObject.transform.GetComponent<Collider>();
+	    rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Move();
+	}
+
+    public void Attack()
+    {
+        
+    }
+
+    public void Move()
+    {
+        float hFloat = Input.GetAxis("Horizontal");
+        Vector3 newPosition = transform.position +  new Vector3(Input.GetAxis("Horizontal")*speed,0, Input.GetAxis("Vertical")*speed);
+        //rigidbody.position + = newPosition;s
+
+        rigidbody.MovePosition(newPosition);
+        //transform.position = newPosition;
+        if (hFloat> 0.1f)
+        {
+            Quaternion turnRight = Quaternion.Euler(new Vector3(0, 90, 0));
+            transform.rotation = turnRight;
+        }
+
+        if (hFloat < -0.1f)
+        {
+            Quaternion turnLeft = Quaternion.Euler(new Vector3(0, -90, 0));
+            transform.rotation = turnLeft;
+        }
+    }
+}
---
+++ Assets/WithinRange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WithinRange : MonoBehaviour
+{
+    int dmg = 5;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy" && Input.GetKeyUp(KeyCode.Space))
+        {
+            AudioClip clip = GetComponent<AudioSource>().clip;
+            GetComponent<AudioSource>().PlayOneShot(clip);
+            other.transform.GetComponent<Enemy>().RecieveDamage(dmg);
+        }
+    }
+
+}//end class

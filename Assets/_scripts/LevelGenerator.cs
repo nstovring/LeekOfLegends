b59989ea408fec552b49c95b1ac78ef1d897413c
nstovring <nstovring@live.dk>
2016-04-09 23:04:17 +0200

    Adding background extra items

--- Assets/_scripts/LevelGenerator.cs
+++ Assets/_scripts/LevelGenerator.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject levelChunck;
     public Sprite[] backGroundSprite= new Sprite[2];
+    public List<Sprite> backgroundObjects = new List<Sprite>(11); 
 	// Use this for initialization
 	void Start ()
 	{
@@ -22,11 +24,28 @@ public class LevelGenerator : MonoBehaviour
             GameObject chunk = Instantiate(levelChunck, newSpawnPos, Quaternion.identity) as GameObject;
 	        if (chunk)
 	        {
-	            int j = (int) Random.Range(0, 2);
-
+	            int j = Random.Range(0, 2);
                 chunk.GetComponentInChildren<SpriteRenderer>().sprite = backGroundSprite[j];
 	            lastSpawnPos = chunk.transform.position;
+	            if (backgroundObjects != null)
+	            {
+	                AddBackgroundObjects(lastSpawnPos);
+	            }
 	        }
 	    }
 	}
+
+    void AddBackgroundObjects(Vector3 centerAreaVector3)
+    {
+        int amountOfObjects = Random.Range(1, 4);
+        for (int i = 0; i < amountOfObjects; i++)
+        {
+            Debug.Log("Spawning Extra stuff");
+            Vector3 objPosition = new Vector3(centerAreaVector3.x + Random.Range(-25, 25), 0, centerAreaVector3.z + Random.Range(-7, 7));
+            Sprite sprite = backgroundObjects[Random.Range(0, 11)];
+            GameObject clone = Instantiate(new GameObject("backGrounDObject"), objPosition, Quaternion.identity) as GameObject;
+            clone.AddComponent<SpriteRenderer>().sprite = sprite;
+            clone.transform.parent = transform;
+        }
+    }
 }
---
+++ Assets/_scripts/VisualEffects.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+public class VisualEffects : MonoBehaviour {
+
+    List<Sprite> soundEffectSprites = new List<Sprite>(10);
+    public Transform soundEffecTransform;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public IEnumerator DisplaySoundEffectS()
+    {
+        SpriteRenderer sRenderer = soundEffecTransform.GetComponent<SpriteRenderer>();
+        sRenderer.sprite = soundEffectSprites[Random.Range(0, soundEffectSprites.Count)];
+        yield return new WaitForSeconds(0.5f);
+        sRenderer.sprite = null;
+    }
+}
